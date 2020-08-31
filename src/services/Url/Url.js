009d1e65f50c372fbe4a42225060c14db2e12365
ayeates <alexander.yeates@selfridges.co.uk>
2020-08-31 19:50:12 +0100

    Refactor URL service

--- src/services/Url/Url.js
+++ src/services/Url/Url.js
@@ -1,32 +1,37 @@
-export const updateURLParam = (key, value) => {
-  const search = new URLSearchParams(window.location.search);
-  value.length ? search.set(key, value) : search.delete(key);
-  const url = search.toString().length > 0 ? `${window.location.pathname}?${search}` : window.location.pathname;
-  window.history.replaceState(null, document.title, url);
-};
+const buildUrl = (search) => {
+  return search.toString().length > 0 ? `${window.location.pathname}?${search}` : window.location.pathname;
+}
 
-export const getURLParam = (key) => {
-  const search = new URLSearchParams(window.location.search);
-  const value = search.get(key);
-  return value;
-};
-
-export const appendURLParam = (key, value) => {
-  const search = new URLSearchParams(window.location.search);
-  const oldValue = search.get(key);
-  if (oldValue && oldValue.includes(value)) return;
-  const newValue = oldValue ? `${oldValue},${value}` : value;
-  search.set(key, newValue);
-  newValue.length ? search.set(key, newValue) : search.delete(key);
-  const url = search.toString().length > 0 ? `${window.location.pathname}?${search}` : window.location.pathname;
+const updateUrl = (url) => {
   window.history.replaceState(null, document.title, url);
 }
 
-export const deleteURLParamValue = (key, value) => {
+const setUrlParam = (key, value) => {
   const search = new URLSearchParams(window.location.search);
-  const oldValue = search.get(key).split(",");
-  const newValue = oldValue.filter(id => id !== value).join(",");
-  newValue ? search.set(key, newValue) : search.delete(key);
-  const url = search.toString().length > 0 ? `${window.location.pathname}?${search}` : window.location.pathname;
-  window.history.replaceState(null, document.title, url);
+  value ? search.set(key, value) : search.delete(key);
+  return search;
+}
+
+export const getURLParam = (key) => {
+  const search = new URLSearchParams(window.location.search);
+  return search.get(key);
+};
+
+export const updateURLParam = (key, value) => {
+  const search = setUrlParam(key, value);
+  const url = buildUrl(search);
+  updateUrl(url);
+};
+
+export const appendURLParam = (key, value) => {
+  const currentValue = getURLParam(key);
+  if (currentValue && currentValue.includes(value)) return;
+  const newValue = currentValue ? `${currentValue},${value}` : value;
+  updateURLParam(key, newValue);
+}
+
+export const deleteURLParamValue = (key, value) => {
+  const currentValueArr = getURLParam(key).split(",");
+  const newValue = currentValueArr.filter(id => id !== value).join(",");
+  updateURLParam(key, newValue);
 }
